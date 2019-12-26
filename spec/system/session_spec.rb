444622f7a5d74eb4bdb325b33975ad2d719a4fd6
Carlos Ignacio Garcia Garcia <charlie.igg@gmail.com>
2019-12-26 17:46:58 +0100

    fixes majority of specs

--- spec/system/session_spec.rb
+++ spec/system/session_spec.rb
@@ -1,27 +1,27 @@
 # frozen_string_literal: true
 
+require 'rails_helper'
+
 RSpec.describe 'Login', type: :system do
   context 'log in' do
-    context 'should be succesful' do
-      user = FactoryBot.create(:user)
-      visit new_user_session_path
-      within('form') do
+    context 'with valid data' do
+      let(:user) { FactoryBot.create(:user) }
+      it 'should be succesful' do
+        visit new_user_session_path
         fill_in 'Email', with: user.email
         fill_in 'Password', with: '12345678'
         click_on('Log in')
+        expect(current_path).to eq root_path
       end
-      expect(current_path).to eq root_path
     end
-
-    context 'should fail' do
-      user = FactoryBot.create(:user)
-      visit new_user_session_path
-      within('form') do
-        fill_in 'Email', with: user.email
+    context 'with invalid data' do
+      it 'should fail' do
+        visit new_user_session_path
+        fill_in 'Email', with: 'non-existent@email.com'
         fill_in 'Password', with: '12345679'
         click_on('Log in')
+        expect(page).to have_content('Log in')
       end
-      expect(page).to have_content('Log in')
     end
   end
 end
